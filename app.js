ce0f1261bae53b74be5f337f93fc66d14d5d33ca
Eyob Shikur <72921005+eyob2one@users.noreply.github.com>
2025-07-01 15:36:40 +0000

    debug

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 app.use(errorHandler);
 
 // Server Configuration
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Start Server
